964c009790ec1c1309175e344d8792f6a9bb6464
Ralf Waspe <rwaspe@icloud.com>
2024-11-12 10:10:53 +0100

    add time stamp to log file

--- WaQtTools/LogFile.h
+++ WaQtTools/LogFile.h
@@ -9,11 +9,12 @@ template <CompileTimeString tag>
 class LogFile
 {
 public:
-   LogFile(const QString& fileName);
+   LogFile(const QString& fileName, bool appendTimeStamp = false);
    ~LogFile();
 
 public:
    static QTextStream stream();
+   QString getFileName() const;
 
 private:
    static LogFile* me;
--- WaQtTools/LogFile.hpp
+++ WaQtTools/LogFile.hpp
@@ -1,18 +1,28 @@
 #ifndef LogFileHPP
 #define LogFileHPP
 
+#include <QDateTime>
+#include <QFileInfo>
+
+
 #include "LogFile.h"
 
 template <CompileTimeString tag>
 inline LogFile<tag>* LogFile<tag>::me = nullptr;
 
 template <CompileTimeString tag>
-inline LogFile<tag>::LogFile(const QString& fileName)
+inline LogFile<tag>::LogFile(const QString& fileName, bool appendTimeStamp)
    : file(nullptr)
 {
    me = this;
 
-   file = new QFile(fileName);
+   const QFileInfo info(fileName);
+   QString logFileName = info.path() + "/" +  info.baseName();
+   if(appendTimeStamp)
+      logFileName +=  QDateTime::currentDateTime().toString("_yyyyMMdd_hhmmss");
+   logFileName += "." + info.completeSuffix();
+
+   file = new QFile(logFileName);
    file->open(QIODevice::WriteOnly);
 }
 
@@ -32,6 +42,12 @@ inline QTextStream LogFile<tag>::stream()
       return QTextStream();
 
    return QTextStream(me->file);
+}
+
+template <CompileTimeString tag>
+QString LogFile<tag>::getFileName() const
+{
+   return file->fileName();
 }
 
 #endif // NOT LogFileHPP
