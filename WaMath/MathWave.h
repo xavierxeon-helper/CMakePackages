67d519eba2d1016a31987d31a1ee4b2cb77da422
Ralf Waspe <rwaspe@icloud.com>
2024-11-18 14:10:03 +0100

    fixed wave

---
+++ WaMath/MathWave.h
@@ -0,0 +1,61 @@
+#ifndef MathWaveH
+#define MathWaveH
+
+// see https://beltoforion.de/en/recreational_mathematics/2d-wave-equation.php
+
+#include <list>
+#include <thread>
+#include <vector>
+
+#include "../WaTools/DoubleBuffer.h"
+#include "../WaTools/Matrix.h"
+#include "MathVector3.h"
+
+namespace Math
+{
+   class Wave
+   {
+   public:
+      Wave(const Size& size, const int& threadCount = 4);
+
+   public:
+      void step(const double& time);
+      const Matrix<double>& getHeightField() const;
+
+      const Size& getSize() const;
+
+      const double& getFrequency() const;
+      void setFrequency(const double& value);
+
+      const double& getSpeed() const;
+      void setSpeed(const double& value);
+
+   private:
+      using LineList = std::list<uint16_t>;
+
+   private:
+      void advanceLines(const uint16_t lineIndex);
+
+   protected:
+      Size size;
+
+
+
+      double frequency;
+      double speed;
+
+      double omega;
+      DoubleBuffer<Matrix<double>> heightField;
+
+   private:
+      int threadCount;
+      std::vector<std::thread> threads;
+      std::vector<LineList> lineLists;
+   };
+} // namespace Math
+
+#ifndef MathWaveHPP
+#include "MathWave.hpp"
+#endif // NOT MathWaveHPP
+
+#endif // NOT MathWaveH
--- WaMath/MathWave.hpp
+++ WaMath/MathWave.hpp
@@ -1,58 +1,106 @@
-#ifndef MathWaveH
-#define MathWaveH
-
-// see https://beltoforion.de/en/recreational_mathematics/2d-wave-equation.php
-
-#include <thread>
-
-#include <DoubleBuffer.h>
-#include <MathVector3.h>
-#include <Matrix.h>
-
-namespace Math
-{
-   class Wave
-   {
-   public:
-      Wave(const Size& size, const int& threadCount = 4);
-
-   public:
-      void step(const double& time);
-      const Matrix<double>& getHeightField() const;
-      const Matrix<Math::Vector3>& getNormals() const;
-
-      const Size& getSize() const;
-
-      const double& getFrequency() const;
-      void setFrequency(const double& value);
-
-      const double& getSpeed() const;
-      void setSpeed(const double& value);
-
-   private:
-      using LineList = QList<quint16>;
-
-   private:
-      void advanceLines(const quint16 lineIndex);
-
-   protected:
-      Size size;
-
-      int threadCount;
-      std::vector<std::thread> threads;
-      QList<LineList> lineLists;
-
-      double frequency;
-      double speed;
-
-      double omega;
-      DoubleBuffer<Matrix<double>> heightField;
-      Matrix<Math::Vector3> normals;
-   };
-} // namespace Math
-
 #ifndef MathWaveHPP
-#include "MathWave.hpp"
-#endif // NOT MathWaveHPP
+#define MathWaveHPP
 
-#endif // NOT MathWaveH
+#include "MathWave.h"
+
+#include <cmath>
+
+inline Math::Wave::Wave(const Size& size, const int& threadCount)
+   : size(size)
+   , threadCount(threadCount)
+   , threads(threadCount)
+   , lineLists()
+   , frequency(1.0)
+   , speed(1.0)
+   , omega(2.0 * M_PI)
+   , heightField(Matrix<double>(size, 0.0), Matrix<double>(size, 0.0))
+{
+   const int maxCount = std::ceil(size.height / threadCount);
+
+   LineList list;
+   for (uint16_t x = 0; x < size.width; x++)
+   {
+      list.push_back(x);
+      if (list.size() >= maxCount)
+      {
+         lineLists.push_back(list);
+         list.clear();
+      }
+   }
+   if (0 < list.size())
+      lineLists.push_back(list);
+}
+
+inline void Math::Wave::step(const double& time)
+{
+   heightField.swap();
+
+   for (uint16_t index = 0; index < threadCount; index++)
+      threads[index] = std::thread(&Wave::advanceLines, this, index);
+
+   for (uint16_t index = 0; index < threadCount; index++)
+      threads[index].join();
+}
+
+inline const Matrix<double>& Math::Wave::getHeightField() const
+{
+   return heightField.current();
+}
+
+inline const Size& Math::Wave::getSize() const
+{
+   return size;
+}
+
+inline const double& Math::Wave::getFrequency() const
+{
+   return frequency;
+}
+
+inline void Math::Wave::setFrequency(const double& value)
+{
+   frequency = value;
+   omega = value * 2.0 * M_PI;
+}
+
+inline const double& Math::Wave::getSpeed() const
+{
+   return speed;
+}
+
+inline void Math::Wave::setSpeed(const double& value)
+{
+   speed = value;
+}
+
+inline void Math::Wave::advanceLines(const quint16 lineIndex)
+{
+   const double damping = 1.0;
+
+   Matrix<double>& current = heightField.current();
+   const Matrix<double>& previous = heightField.previous();
+
+   const LineList& lineList = lineLists[lineIndex];
+   for (const uint16_t x : lineList)
+   {
+      for (uint16_t y = 0; y < size.height; y++)
+      {
+         double height = 0.0;
+         const double center = current[x][y];
+
+         if (x > 0)
+            height += previous[x - 1][y];
+         if (x + 1 < size.width)
+            height += previous[x + 1][y];
+         if (y > 0)
+            height += previous[x][y - 1];
+         if (y + 1 < size.height)
+            height += previous[x][y + 1];
+
+         current[x][y] = (0.5 * height) - center;
+         current[x][y] *= damping;
+      }
+   }
+}
+
+#endif // NOT MathWaveHPP
