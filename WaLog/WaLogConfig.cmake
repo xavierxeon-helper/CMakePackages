02dbe05017363b501af883c73f2878f7c0cc3a68
Ralf Waspe <rwaspe@icloud.com>
2024-12-01 22:54:14 +0100

    set c++ version

--- WaLog/WaLogConfig.cmake
+++ WaLog/WaLogConfig.cmake
@@ -1,9 +1,12 @@
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+if(APPLE)
+   set(CMAKE_OSX_DEPLOYMENT_TARGET "14.0") # for Qt
+endif()
+
 # Compute paths
 get_filename_component(WALOG_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 set(WALOG_INCLUDE_DIRS ${WALOG_CMAKE_DIR})
 
 include_directories(${WALOG_INCLUDE_DIRS})
-
-if(APPLE)
-   set(CMAKE_OSX_DEPLOYMENT_TARGET "14.0") # for Qt
-endif()
--- WaMath/WaMathConfig.cmake
+++ WaMath/WaMathConfig.cmake
@@ -1,9 +1,12 @@
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+if(APPLE)
+   set(CMAKE_OSX_DEPLOYMENT_TARGET "14.0") # for Qt
+endif()
+
 # Compute paths
 get_filename_component(WAMATH_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 set(WAMATHS_INCLUDE_DIRS ${WAMATH_CMAKE_DIR})
 
 include_directories(${WAMATHS_INCLUDE_DIRS})
-
-if(APPLE)
-   set(CMAKE_OSX_DEPLOYMENT_TARGET "14.0") # for Qt
-endif()
