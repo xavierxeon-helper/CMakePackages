9354995d59f460f1a7a26345ee7a95e116daf5b0
Ralf Waspe <rwaspe@icloud.com>
2025-01-22 14:53:14 +0100

    crypto stuff

---
+++ WaCrypt/WaCryptConfig.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+if(APPLE)
+   set(CMAKE_OSX_DEPLOYMENT_TARGET "14.0") # for Qt
+endif()
+
+# Compute paths
+get_filename_component(WACRYPT_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+set(WACRYPT_INCLUDE_DIRS ${WACRYPT_CMAKE_DIR})
+
+include_directories(${WACRYPT_INCLUDE_DIRS})
