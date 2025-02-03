f6e03c5bc4631661f69dd89007554a275061e446
Ralf Waspe <rwaspe@icloud.com>
2025-02-03 09:21:59 +0100

    split function into seperate files

--- WaTools/WaToolsConfig.cmake
+++ WaTools/WaToolsConfig.cmake
@@ -10,4 +10,5 @@ get_filename_component(WATOOLS_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 set(WATOOLS_INCLUDE_DIRS ${WATOOLS_CMAKE_DIR})
 
 include_directories(${WATOOLS_INCLUDE_DIRS})
-include(${WATOOLS_CMAKE_DIR}/functions.cmake)
+include(${WATOOLS_CMAKE_DIR}/functions_compile.cmake)
+include(${WATOOLS_CMAKE_DIR}/functions_target.cmake)
