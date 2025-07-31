dfd53dfd1798fa3b7e4ec7d31d3c3da5e640a015
Ralf Waspe <rwaspe@icloud.com>
2025-07-31 15:48:58 +0200

    docu stuff

--- create_docu.sh
+++ create_docu.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+HERE=$(dirname "$(readlink -f "$0")")
+
 doxygen CMakePackage.doxy
+
+cd $HERE/documentation/latex
+make
+
+cp $HERE/documentation/latex/refman.pdf $HERE/CMakePackage.pdf
