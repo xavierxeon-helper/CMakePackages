e585779ff5779530de788aaed8f3380626cf99ce
Ralf Waspe <rwaspe@icloud.com>
2025-08-18 00:04:01 +0200

    do not create pdf doku

--- docu.sh
+++ docu.sh
@@ -2,13 +2,10 @@
 
 HERE=$(dirname "$(readlink -f "$0")")
 
-PDF_TEST=$(which pdflatex)
-if [ ! -z "$PDF_TEST" ]; then
-   cd $HERE
-   doxygen CMakePackage.doxy
-   cd $HERE/documentation/latex
-   make
-   cp $HERE/documentation/latex/refman.pdf $HERE/CMakePackage.pdf
-fi
-
 cd $HERE
+doxygen CMakePackage.doxy
+
+#cd $HERE/documentation/latex
+#make
+#cp $HERE/documentation/latex/refman.pdf $HERE/CMakePackage.pdf
+#cd $HERE
