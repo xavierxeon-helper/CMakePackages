797c1158f1db8a7315536efd2873690b6f6cacc1
Ralf Waspe <rwaspe@icloud.com>
2025-08-18 04:46:58 +0200

    cleanup build

--- create.sh
+++ create.sh
@@ -4,12 +4,10 @@ HERE=$(dirname "$(readlink -f "$0")")
 
 mkdir -p $HERE/build/cmd_line_release
 cd $HERE/build/cmd_line_release
-cmake  $HERE -DCMAKE_BUILD_TYPE=Release
-make
+cmake  $HERE  -DCMAKE_BUILD_TYPE=Release
+cmake --build . --parallel --config Release
 
 mkdir -p $HERE/build/cmd_line_debug
 cd $HERE/build/cmd_line_debug
-cmake $HERE -DCMAKE_BUILD_TYPE=Debug
-make
-
-cd $HERE
+cmake  $HERE  -DCMAKE_BUILD_TYPE=Debug
+cmake --build . --parallel --config Debug
