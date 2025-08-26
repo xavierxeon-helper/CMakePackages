b76ae51a32d4ee4e601ade874f3ddeaac560efd4
Ralf Waspe <rwaspe@icloud.com>
2025-08-26 11:22:54 +0200

    try docu

--- docu.sh
+++ docu.sh
@@ -3,9 +3,16 @@
 HERE=$(dirname "$(readlink -f "$0")")
 
 cd $HERE
-doxygen CMakePackage.doxy
+doxygen WaTools.doxy
 
-#cd $HERE/documentation/latex
-#make
-#cp $HERE/documentation/latex/refman.pdf $HERE/CMakePackage.pdf
-#cd $HERE
+echo "create PDF (y/N)?"
+read CREATE_PDF
+if [ "$CREATE_PDF" == "y" ] 
+then
+   cd $HERE/documentation/latex
+   make
+
+   cp $HERE/documentation/latex/refman.pdf $HERE/documentation/WaTools.pdf
+   cd $HERE
+fi
+
