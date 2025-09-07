6c7456946d5d4b348b3ffac26e1dc08f738c32a7
Ralf Waspe <rwaspe@icloud.com>
2025-09-07 16:36:42 +0200

    split deploy function

--- WaTools/functions_qt.cmake
+++ WaTools/functions_qt.cmake
@@ -43,12 +43,35 @@ function(add_qml_source_dir SUBPATH)
    )
 endfunction()
 
+function(run_watools_deploy)
+   if(NOT CMAKE_BUILD_TYPE STREQUAL "Release")
+      return()
+   endif()
+
+   if(APPLE)
+      file(GLOB WATOOLS_FILES
+         ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../lib/release/*.so
+      )
+   elseif(WIN32)
+      file(GLOB WATOOLS_FILES
+         ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../lib/release/*.dll
+      )
+   endif()
+
+   add_custom_command(TARGET ${PROJECT_NAME}
+      POST_BUILD
+      COMMENT "copy watools ..."
+      COMMAND ${CMAKE_COMMAND} -E copy ${WATOOLS_FILES} $<TARGET_FILE_DIR:${PROJECT_NAME}>
+   )
+
+endfunction()
+
+
 # qt deploy , see https://www.qt.io/blog/cmake-deployment-api
 function(run_qt_deploy)
    if(NOT CMAKE_BUILD_TYPE STREQUAL "Release")
       return()
    endif()
-
    
    get_target_property(QMAKE_EXE Qt6::qmake IMPORTED_LOCATION)
    get_filename_component(QT_BIN_DIR "${QMAKE_EXE}" DIRECTORY)
@@ -64,10 +87,6 @@ function(run_qt_deploy)
          COMMENT "Running macdeployqt..."
          COMMAND "${MACDEPLOYQT_EXECUTABLE}"  $<TARGET_BUNDLE_DIR:${PROJECT_NAME}> -qmldir ${QT_QML_DIR}
       )
-
-      file(GLOB WATOOLS_FILES
-         ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../lib/release/*.so
-      )
    elseif(WIN32)
       find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS " ${QT_BIN_DIR} ")
       message(STATUS " WINDEPLOY ${WINDEPLOYQT_EXECUTABLE}, QT_QML_DIR ${QT_QML_DIR} ")
@@ -77,19 +96,6 @@ function(run_qt_deploy)
          COMMENT "Running windeployqt... "
          COMMAND "${WINDEPLOYQT_EXECUTABLE}" --no-translations --no-system-d3d-compiler --compiler-runtime --qmldir ${QT_QML_DIR} $<TARGET_FILE:${PROJECT_NAME}>
       )
-
-      file(GLOB WATOOLS_FILES
-         ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../lib/release/*.dll
-      )
-
    endif()
-
-   add_custom_command(TARGET ${PROJECT_NAME}
-      POST_BUILD
-      COMMENT "copy watools ..."
-      COMMAND ${CMAKE_COMMAND} -E copy
-               ${WATOOLS_FILES}
-               $<TARGET_FILE_DIR:${PROJECT_NAME}>
-   )
 
 endfunction()
