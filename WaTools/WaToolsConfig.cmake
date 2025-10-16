dbd3a719df2f1a58c682e7b9937630bd58b4e168
Ralf Waspe <rwaspe@icloud.com>
2025-10-16 10:38:46 +0200

    split functions file

--- WaTools/WaToolsConfig.cmake
+++ WaTools/WaToolsConfig.cmake
@@ -1,7 +1,8 @@
 include(${CMAKE_CURRENT_LIST_DIR}/functions_compile.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/functions_qt.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/functions_target.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/functions_deploy.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/functions_git.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/functions_qml.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/functions_target.cmake)
 
 foreach(COMPONENT ${WaTools_FIND_COMPONENTS})
    #message(STATUS "NEED WaTools component: ${COMPONENT}")
