292a2a9aeb22c7ce043a4e2db8736b00b4a7e18b
Ralf Waspe <rwaspe@icloud.com>
2026-02-06 10:58:43 +0100

    raspi test

--- update.sh
+++ update.sh
@@ -9,8 +9,14 @@ fi
 PLATFORM=$(uname -ms)
 echo "Platform: $PLATFORM"
 
-PLATFORM_RASPI="armv7l GNU/Linux"
+PLATFORM_RASPI="Linux aarch64"
 echo "Platform: $PLATFORM_RASPI"
+
+
+if [ "$PLATFORM" == "$PLATFORM_RASPI" ]
+then
+   echo "Running on Raspberry Pi, skipping parallel build"
+fi
 exit 0
 
 HERE=$(dirname "$(readlink -f "$0")")
@@ -18,11 +24,21 @@ HERE=$(dirname "$(readlink -f "$0")")
 mkdir -p $HERE/build/cmd_line_debug
 cd $HERE/build/cmd_line_debug
 cmake  $HERE  -DCMAKE_BUILD_TYPE=Debug
-cmake --build . --parallel --config Debug
+
+if [ "$PLATFORM" == "$PLATFORM_RASPI" ]
+then
+    cmake --build . --config Debug
+else
+   cmake --build . --parallel --config Debug
 
 mkdir -p $HERE/build/cmd_line_release
 cd $HERE/build/cmd_line_release
 cmake  $HERE  -DCMAKE_BUILD_TYPE=Release
-cmake --build . --parallel --config Release
+
+if [ "$PLATFORM" == "$PLATFORM_RASPI" ]
+then
+    cmake --build . --config Release
+else
+   cmake --build . --parallel --config Release
 
 
