e85043dc7958de964b1c5325712540a40108c8a3
Ralf Waspe <raw@mmmi.sdu.dk>
2026-03-11 20:59:34 +0100

    windows compile

--- XXCore/XXCoreConfig.cmake
+++ XXCore/XXCoreConfig.cmake
@@ -4,3 +4,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/../CommonUse.cmake)
 find_package(Qt6 REQUIRED COMPONENTS Core)
 link_libraries(Qt6::Core XXCore)
 
+if(EMSCRIPTEN)
+   link_libraries(idbfs.js)
+endif()
--- XXNetworkOAuth/XXNetworkOAuthConfig.cmake
+++ XXNetworkOAuth/XXNetworkOAuthConfig.cmake
@@ -1,6 +1,6 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 include(${CMAKE_CURRENT_LIST_DIR}/../CommonUse.cmake)
 
-find_package(Qt6 REQUIRED COMPONENTS Core)
-link_libraries(Qt6::Core XXNetworkOAuth)
+find_package(Qt6 REQUIRED COMPONENTS NetworkAuth)
+link_libraries(Qt6::NetworkAuth XXNetworkOAuth)
 
