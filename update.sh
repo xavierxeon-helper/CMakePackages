e74eec4f5821db1a7fd9f5c33d913dca9d2f0760
Ralf Waspe <raw@mmmi.sdu.dk>
2026-05-06 08:40:04 +0200

    preint build time

--- update.sh
+++ update.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
+START_TIME=$(date +%s.%N)
+
 if [[ -f /proc/version && $(grep -i Microsoft /proc/version) && $1 != "--wsl" ]]
 then
    cmd.exe /c "windows_msvc_update.bat"
-   exit 0
+else
+   PLATFORM=$(uname -ms)
+   PLATFORM_RASPI="Linux aarch64"
+
+   if [ "$PLATFORM" == "$PLATFORM_RASPI" ]
+   then   
+      echo "Running on Raspberry Pi, do not use parallel build"
+   else   
+      PARALLEL_BUILD=--parallel
+   fi
+
+   HERE=$(dirname "$(readlink -f "$0")")
+
+   mkdir -p $HERE/build/unix_debug
+   cd $HERE/build/unix_debug
+   cmake $HERE -DCMAKE_BUILD_TYPE=Debug
+
+   cmake --build . $PARALLEL_BUILD --config Debug
+
+   mkdir -p $HERE/build/unix_release
+   cd $HERE/build/unix_release
+   cmake $HERE -DCMAKE_BUILD_TYPE=Release
+
+   cmake --build . $PARALLEL_BUILD --config Release
 fi
 
-PLATFORM=$(uname -ms)
-PLATFORM_RASPI="Linux aarch64"
-
-if [ "$PLATFORM" == "$PLATFORM_RASPI" ]
-then   
-   echo "Running on Raspberry Pi, do not use parallel build"
-else   
-   PARALLEL_BUILD=--parallel
-fi
-
-HERE=$(dirname "$(readlink -f "$0")")
-
-mkdir -p $HERE/build/unix_debug
-cd $HERE/build/unix_debug
-cmake $HERE -DCMAKE_BUILD_TYPE=Debug
-
-cmake --build . $PARALLEL_BUILD --config Debug
-
-mkdir -p $HERE/build/unix_release
-cd $HERE/build/unix_release
-cmake $HERE -DCMAKE_BUILD_TYPE=Release
-
-cmake --build . $PARALLEL_BUILD --config Release
+END_TIME=$(date +%s.%N)
+echo ""
+echo "Build time: $(echo "$END_TIME - $START_TIME" | bc) seconds"
