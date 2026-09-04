1d6c94fc1ab015a268dc590adfc0bcef04b6699b
Ralf Waspe <raw@mmmi.sdu.dk>
2026-09-04 13:36:20 +0200

    copy python dependencies

--- PythonBindings/PythonBindingsConfig.cmake
+++ PythonBindings/PythonBindingsConfig.cmake
@@ -10,3 +10,11 @@ file(TO_CMAKE_PATH "${pybind11_DIR}" pybind11_DIR)
 find_package(pybind11 3.0 REQUIRED)
 
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+function(copy_python_qt_dependenies)
+   add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            $<TARGET_RUNTIME_DLLS:${PROJECT_NAME}> $<TARGET_FILE_DIR:${PROJECT_NAME}>
+      COMMAND_EXPAND_LISTS
+   )
+endfunction()
