0e1025a98359ebb7caff6b76875ed332643a7d29
Ralf Waspe <raw@mmmi.sdu.dk>
2026-09-04 16:58:56 +0200

    python changes

--- PythonBindings/PythonBindingsConfig.cmake
+++ PythonBindings/PythonBindingsConfig.cmake
@@ -1,4 +1,4 @@
-# pybind stuff
+#
 find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
 
 execute_process(
@@ -12,9 +12,11 @@ find_package(pybind11 3.0 REQUIRED)
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 function(copy_python_qt_dependenies)
-   add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            $<TARGET_RUNTIME_DLLS:${PROJECT_NAME}> $<TARGET_FILE_DIR:${PROJECT_NAME}>
-      COMMAND_EXPAND_LISTS
-   )
+   if(WIN32)
+      add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+         COMMAND ${CMAKE_COMMAND} -E copy_if_different
+               $<TARGET_RUNTIME_DLLS:${PROJECT_NAME}> $<TARGET_FILE_DIR:${PROJECT_NAME}>
+         COMMAND_EXPAND_LISTS
+      )
+   endif()
 endfunction()
